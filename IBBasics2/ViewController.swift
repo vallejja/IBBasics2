4fae02d91db7af77ab3188f83e87384919a6482a
Julio Vallejo <vallejja@ML-C1MTD3D6H3QF.local>
2017-10-28 00:27:37 -0400

    Initial Commit

---
+++ IBBasics2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBBasics2
+//
+//  Created by Julio Vallejo on 2017-10-28.
+//  Copyright © 2017 Julio Vallejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
